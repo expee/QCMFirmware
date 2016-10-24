4e1b2df396f225cbe8ecd72b5b855337624f7032
expee <experian.elitiawan@gmail.com>
2016-10-24 23:44:45 +0700

    * Data direction has been correctly assigned * PIN and PORT function definition has been correctly assigned * i2c command processing routine has been implemented + command set has been added * external interrupt INT0 has been enabled

--- QCMSlaveReference/QCMSlaveReference/main.c
+++ QCMSlaveReference/QCMSlaveReference/main.c
@@ -10,39 +10,38 @@
 #include <avr/interrupt.h>
 #include "../../CommonLibs/i2c_atmega.h"
 
-#define RS_DATA		PORTA|=1<<PORTA2
-#define RS_INST		PORTA&=~(1<<PORTA2)
-#define EN_ON		PORTA|=1<<PORTA0
-#define EN_OFF		PORTA&=~(1<<PORTA0)
-#define RW_READ		PORTA|=1<<PORTA1
-#define RW_WRITE	PORTA&=~(1<<PORTA1)
-#define D7_HIGH		PORTA|=1<<PORTA7
-#define D6_HIGH		PORTA|=1<<PORTA6
-#define D5_HIGH		PORTA|=1<<PORTA5
-#define D4_HIGH		PORTA|=1<<PORTA4
-#define D7_LOW		PORTA&=~(1<<PORTA7)
-#define D6_LOW		PORTA&=~(1<<PORTA6)
-#define D5_LOW		PORTA&=~(1<<PORTA5)
-#define D4_LOW		PORTA&=~(1<<PORTA4)
+#define LE_HIGH		PORTB|=1<<PB0
+#define MR_HIGH		PORTB|=1<<PB1
+#define S1_HIGH		PORTB|=1<<PB2
+#define	S2_HIGH		PORTB|=1<<PB3
+#define S3_HIGH		PORTB|=1<<PB4
+#define LE_LOW		PORTB&=~(1<<PB0)
+#define MR_LOW		PORTB&=~(1<<PB1)
+#define S1_LOW		PORTB&=~(1<<PB2)
+#define	S2_LOW		PORTB&=~(1<<PB3)
+#define S3_LOW		PORTB&=~(1<<PB4)
 
-#define LE_HIGH		PORTC|=1<<PORTC1
-#define MR_HIGH		PORTC|=1<<PORTC0
-#define S1_HIGH		PORTC|=1<<PORTC7
-#define	S2_HIGH		PORTC|=1<<PORTC6
-#define S3_HIGH		PORTC|=1<<PORTC5
-#define LE_LOW		PORTC&=~(1<<PORTC1)
-#define MR_LOW		PORTC&=~(1<<PORTC0)
-#define S1_LOW		PORTC&=~(1<<PORTC7)
-#define	S2_LOW		PORTC&=~(1<<PORTC6)
-#define S3_LOW		PORTC&=~(1<<PORTC5)
+#define Z1			((PINC>>PC5)&0x01)
+#define Z2			((PINC>>PC6)&0x01)
+#define Z3			((PINC>>PC7)&0x01)
 
-#define Z1			((PINC>>PORTC2)&0x01)
-#define Z2			((PINC>>PORTC3)&0x01)
-#define Z3			((PINC>>PORTC4)&0x01)
+enum commandSet
+{
+	NOP = 0,
+	SEND_MEASUREMENT_DATA,
+	SEND_THERMAL_DATA,
+	TEST_COMM
+};
 
-volatile unsigned char x = 0;
-volatile uint32_t freqVal = 100000, freqBuff0 = 0, freqBuff1 = 0, freqBuff2 = 0;
-volatile unsigned char swt = 0;
+typedef union commData
+{
+	volatile uint32_t freqVal;
+	volatile uint8_t dataTrain[4];
+}commData_t;
+
+volatile commData_t dataToSend;
+
+volatile uint32_t freqBuff0 = 0, freqBuff1 = 0, freqBuff2 = 0;
 
 //==============================I2C FUNCTIONS=============================================
 void setSpecificI2c_prepComm (uint8_t cmd, uint8_t* payLoad, uint8_t payLoadSize)
@@ -57,26 +56,39 @@ void setSpecificI2c_restartDataDir(uint8_t cmd)
 
 void i2c_processCommand(uint8_t cmd)
 {
-	//TODO : define this function body
+	uint8_t i = 0;
+	switch (cmd)
+	{
+		case SEND_MEASUREMENT_DATA:
+		{
+			for (i = 0; i < sizeof(commData_t); i++)
+			{
+				s_payLoad[i] = dataToSend.dataTrain[i];
+			}
+		}break;
+		case SEND_THERMAL_DATA:
+		{
+			//NOP
+		}break;
+		case TEST_COMM:
+		{
+			//NOP
+		}break;
+	}
 }
 //==============================I2C FUNCTIONS=============================================
 
 //==============================TIMER FUNCTIONS=============================================
-void initTimer1 ()
-{
-	//TODO : Make the right initialization
-	TCCR1A = 0x00;
-	TCCR1B = 0x05;
-	TIMSK |=(1<<TOIE1);
-	TCNT1 = 0xc2f7;
-}
+
 //==============================TIMER FUNCTIONS=============================================
 
 void initExternalInt (void)
 {
-	
+	//INT0 triggers on falling edge
+	MCUCR |= (1<<ISC01)|(1<<ISC00);
+	//Activate INT0 Interrupt
+	GICR |=(1<<INT0);
 }
-
 
 ISR(INT0_vect)
 {
@@ -94,7 +106,7 @@ ISR(INT0_vect)
 		freqBuff1|=Z2<<i;
 		freqBuff2|=Z3<<i;
 	}
-	freqVal = (freqBuff2 << 16)|(freqBuff1 << 8)|(freqBuff0);
+	dataToSend.freqVal = (freqBuff2 << 16)|(freqBuff1 << 8)|(freqBuff0);
 	freqBuff0 = 0;
 	freqBuff1 = 0;
 	freqBuff2 = 0;
@@ -102,16 +114,14 @@ ISR(INT0_vect)
 
 int main(void)
 {
-	//TODO : correct these
 	DDRA = 0x00;
-	DDRB = 0x00;
+	DDRB = 0x1f;
 	DDRC = 0x00;
 	DDRD = 0x00;
-
 	PORTA = 0x00;
 	PORTB = 0x00;
 	PORTC = 0x00;
-	PORTD = 0x00;
+	PORTD = 0x04;	//Enable pull-up resistor on PD2
 	sei();
 	i2c_Init(300000,0x51);
 	/* Replace with your application code */
